2a7642e1fd225d1bd062a1c95bd9bf2989eb47e1
soumik7063 <ghataksoumik7063@gmail.com>
2026-02-05 00:52:54 +0530

    bug fixed

--- components/upload-thing/Upload_form.tsx
+++ components/upload-thing/Upload_form.tsx
@@ -41,7 +41,6 @@ const Upload_form = () => {
       const valid = schema.safeParse({ file });
       if (!valid.success) {
         toast.error(valid.error.errors[0].message);
-        setGenerating(false);
         return;
       }
 
@@ -67,9 +66,8 @@ const Upload_form = () => {
         // console.log(storeResult);
         toast("✅ saved successfully");
       }
-      setSumId(storeResult.data.id);
+      setSumId(storeResult?.data?.id);
     } catch (error) {
-      setGenerating(false);
       console.log("error occured: ", error);
     } finally {
       setGenerating(false);
@@ -85,7 +83,7 @@ const Upload_form = () => {
           <p className="text-indigo-400 animate-pulse">Generating summary…</p>
         )}
 
-        {output && (
+        {output && sumId && (
           <div className="bg-neutral-900 border border-indigo-500/20 rounded-xl p-8 space-y-6">
             <h2 className="text-2xl font-bold text-indigo-400">
               Your PDF Summary
